6a45f73766ed5e8095f812dc72c1217ee0c4d25e
s1140422-ui <s1140422@dmhs.kh.edu.tw>
2026-05-22 10:56:56 +0800

    Create escape.cpp

---
+++ escape.cpp
@@ -0,0 +1,12 @@
+1.快樂YBY
+2.快樂 YBY
+3.快樂  YBY
+4.快樂
+YBY
+"5.快樂YBY"
+'6.快樂YBY'
+\7.快樂YBY\
+
+--------------------------------
+Process exited after 0.004614 seconds with return value 0
+請按任意鍵繼續 . . .
